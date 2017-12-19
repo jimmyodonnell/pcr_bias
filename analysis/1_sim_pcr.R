ee64227878f02668fa007a2c66e876a6a2b558dc
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-19 01:22:39 -0800

    incorporate multiple variances of beta efficiency

--- analysis/1_sim_pcr.R
+++ analysis/1_sim_pcr.R
@@ -2,24 +2,38 @@
 # simulate PCR
 ################################################################################
 
-library(data.table)
-
 # Primer efficiency will be modified, though original sample will not, 
 # so add primer efficiency now rather than generate fixed efficiencies upfront
-set.seed(1)
-template_dat[, eff := primer_eff(N = templates, mmv = 'custom', mmv.beta = c(9.5,0.5)), by = sample]
 
-pcr_reps <- 1 # pointless when stochastic = FALSE...
-set.seed(1) # pointless when stochastic = FALSE; remains just in case.
-temp <- list()
-for(i in 1:pcr_reps){
-  temp[[i]] <- template_dat[,list(
-    rep.pcr = i, 
-    species, 
-    amplicons = do_pcr(template_copies = templates, 
-                       template_effs = eff, 
-                       ncycles = 30, inflection = 15, slope = 0.5, stochastic = FALSE)
-    ), by = sample]
+library(data.table)
+
+eff_mean <- 0.95
+var_lev <- c(1, 10, 100, 1000)
+
+pcr_dat <- list()
+set.seed(1)
+for(v in 1:length(var_lev)){
+  SHAPE1 <- eff_mean * var_lev[v]
+  SHAPE2 <- (1 - eff_mean) * var_lev[v]
+  template_dat[, eff := primer_eff(
+    N = templates, mmv = 'custom', mmv.beta = c(SHAPE1, SHAPE2)), 
+    by = sample]
+
+  pcr_reps <- 1 # pointless when stochastic = FALSE...
+  set.seed(1) # pointless when stochastic = FALSE; remains just in case.
+  temp <- list()
+  for(i in 1:pcr_reps){
+    temp[[i]] <- template_dat[,list(
+      rep.pcr = i, 
+      mmv = var_lev[v], 
+      species, 
+      amplicons = do_pcr(template_copies = templates, 
+                         template_effs = eff, 
+                         ncycles = 30, inflection = 15, slope = 0.5, stochastic = FALSE)
+      ), by = sample]
+  }
+  pcr_dat[[v]] <- rbindlist(temp)
+  rm(temp)
 }
-pcr_dat <- rbindlist(temp)
-rm(temp)
+
+pcr_dat <- rbindlist(pcr_dat)
